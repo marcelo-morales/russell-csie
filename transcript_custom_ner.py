262a7352c4ad2689b9ee2cf4e115f1b8c3c01b8c
marcelomorales <lmoral10@jhu.edu>
2022-03-11 12:44:02 -0500

    combining audio input with program

--- transcript_custom_ner.py
+++ transcript_custom_ner.py
@@ -73,32 +73,45 @@ if __name__ == "__main__":
 
     # training data
     TRAIN_DATA = [
-                ("Walmart is a leading e-commerce company", {"entities": [(0, 7, "ORG")]}),
-                ("I reached Chennai yesterday.", {"entities": [(19, 28, "GPE")]}),
-                ("I recently ordered a book from Amazon", {"entities": [(24,32, "ORG")]}),
-                ("I was driving a BMW", {"entities": [(16,19, "PRODUCT")]}),
-                ("I ordered this from ShopClues", {"entities": [(20,29, "ORG")]}),
-                ("Fridge can be ordered in Amazon ", {"entities": [(0,6, "PRODUCT")]}),
-                ("I bought a new Washer", {"entities": [(16,22, "PRODUCT")]}),
-                ("I bought a old table", {"entities": [(16,21, "PRODUCT")]}),
-                ("I bought a fancy dress", {"entities": [(18,23, "PRODUCT")]}),
-                ("I rented a camera", {"entities": [(12,18, "PRODUCT")]}),
-                ("I rented a tent for our trip", {"entities": [(12,16, "PRODUCT")]}),
-                ("I rented a screwdriver from our neighbour", {"entities": [(12,22, "PRODUCT")]}),
-                ("I repaired my computer", {"entities": [(15,23, "PRODUCT")]}),
-                ("I got my clock fixed", {"entities": [(16,21, "PRODUCT")]}),
-                ("I got my truck fixed", {"entities": [(16,21, "PRODUCT")]}),
-                ("Flipkart started it's journey from zero", {"entities": [(0,8, "ORG")]}),
-                ("I recently ordered from Max", {"entities": [(24,27, "ORG")]}),
-                ("Flipkart is recognized as leader in market",{"entities": [(0,8, "ORG")]}),
-                ("I recently ordered from Swiggy", {"entities": [(24,29, "ORG")]})
-                ]
+              ("Is your person wearing glasses?", {"entities": [(23,30,"glasses")]}),
+              ("Do they have glasses?", {"entities": [(13,20,"glasses")]}),
+              ("Does your person have glasses on?", {"entities": [(22,29,"glasses")]}),
+
+              ("Is your person four-eyed?", {"entities": [(15,24,"glasses")]}),
+              ("Is she four-eyed?", {"entities": [(7,16,"glasses")]}),
+
+              ("Is she blond?", {"entities": [(7,12,"hair_color")]}),
+              ("Is your person blond-haired?", {"entities": [(15,20,"hair_color")]}),
+              ("Is he golden-haired?", {"entities": [(6,12,"hair_color")]}),
+              ("Are they gold-haired?", {"entities": [(9,13,"hair_color")]}),
+              ("Does your person have yellow hair?", {"entities": [(22,28,"hair_color")]}),
+              ("Are they auburn-haired?", {"entities": [(9,15,"hair_color")]}),
+              ("Is your person a ginger?", {"entities": [(17,24,"hair_color")]}),              
+              
+              ("Is your person wearing a green hat?", {"entities": [(25,30,"hat_color")]}),
+              ("Are they wearing a green hat?", {"entities": [(19,24,"hat_color")]}),
+              ("Does your person have a green hat?", {"entities": [(24,29,"hat_color")]}), 
+              ("Do they have a green hat?", {"entities": [(15,20,"hat_color")]}),
+
+              ("Does your person wear a hat?", {"entities": [(24,27,"hat")]}), 
+              ("Do they wear a hat?", {"entities": [(15,18,"hat")]}), 
+              ("Do they have a hat?", {"entities": [(15,18,"hat")]}), 
+
+              ("Does your person not have head hair?", {"entities": [(17,35,"bald")]}),
+              ("Is your person bald?", {"entities": [(15,19,"bald")]}),
+              ("Is she bald?", {"entities": [(7,13,"bald")]}),
+              ("Is he bald?", {"entities": [(6,10,"bald")]}),
+              ("Does he not have head hair?", {"entities": [(8,26,"bald")]}),
+              ("Does he not have hair on his head?", {"entities": [(8,33,"bald")]})                                          
+              # ("Walmart is a leading e-commerce company", {"entities": [(0, 7, "ORG")]})
+              ]
 
     # Adding labels to the `ner`
 
     for _, annotations in TRAIN_DATA:
         for ent in annotations.get("entities"):
             ner.add_label(ent[2])
+    # print("adding label")
 
     # Disable pipeline components you dont need to change
     pipe_exceptions = ["ner", "trf_wordpiecer", "trf_tok2vec"]
@@ -175,8 +188,8 @@ if __name__ == "__main__":
                             annotations,  # batch of annotations
                             drop=0.5,  # dropout - make it harder to memorise data
                             losses=losses,
-                        )
-            print("Losses", losses)
+                )
+    print("Losses", losses)
 
 
 
@@ -189,8 +202,6 @@ if __name__ == "__main__":
     #print(stopwords.words('english'))
     #words_to_filter = set(stopwords.words('english'))
     
-    
-
     instruction = "ask me question based on a specific attribute for my character"
     print(instruction)
     time.sleep(1)
@@ -222,7 +233,8 @@ if __name__ == "__main__":
 
 
     print("this is actual trascription " + response_from_user["transcription"])
-    transcription = "Do they have blond hair and purple glasses?"
+    #response form user passed to transcription variable
+    transcription = response_from_user["transcription"]
 
     # Testing the model
     doc = nlp(transcription)
@@ -234,7 +246,7 @@ if __name__ == "__main__":
 
     displacy.render(doc, style='ent',jupyter=True)
 
-    ## ent.text ('blond') and ent.label ('DATE', which will ultimately be 'hair_color') will then be sent to the game backend to check
+    ## ent.text ('blond') and ent.label ( 'hair_color') will then be sent to the game backend to check
     ##
     ## guess_trait = ent.label
     ## guess_adj = ent.text

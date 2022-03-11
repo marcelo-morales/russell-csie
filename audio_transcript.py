d008f1325dbbcac7b387c974625d7f7a50ddc1d4
marcelomorales <lmoral10@jhu.edu>
2022-03-11 11:44:43 -0500

    combine audio transcript with spacey ner

--- audio_transcript.py
+++ audio_transcript.py
@@ -1,11 +1,13 @@
 import speech_recognition as sr
 import time
-from nltk.corpus import stopwords 
-from nltk.tokenize import word_tokenize
+#from gensim.parsing.preprocessing import remove_stopwords
+#look into finding a way to remove stop words without anaconda, installation issues
 
 #using speech_recognition library
 #tutorial: https://realpython.com/python-speech-recognition/
 
+#input as a string
+#microphone low
 def recognize_speech(recognizer, microphone):
     if not isinstance(recognizer, sr.Recognizer):
         raise TypeError("`recognizer` must be `Recognizer` instance")
@@ -47,16 +49,16 @@ if __name__ == "__main__":
     microphone = sr.Microphone()
 
     print("these are the stopwords i will use \n")
-    print(stopwords.words('english'))
-    words_to_filter = set(stopwords.words('english'))
+   # print(stopwords.words('english'))
+    #words_to_filter = set(stopwords.words('english'))
     
     
 
     instruction = "ask me question based on a specific attribute for my character"
     print(instruction)
-    time.sleep(3)
+    #time.sleep(2)
 
-    PROMPT_LIMIT = 3 #number of times a user is allowed to speak to microphone
+    PROMPT_LIMIT = 1 #number of times a user is allowed to speak to microphone
 
     for i in range(PROMPT_LIMIT):
         response_from_user = recognize_speech(recognizer, microphone)
@@ -67,18 +69,16 @@ if __name__ == "__main__":
 
     print("You said: {}".format(response_from_user["transcription"]))
 
-    word_tokens = word_tokenize(response_from_user["transcription"])
+    # word_tokens = word_tokenize(response_from_user["transcription"])
 
-    filtered_sentence = [w for w in word_tokens if not w.lower() in words_to_filter]
+    # filtered_sentence = [w for w in word_tokens if not w.lower() in words_to_filter]
  
-    filtered_sentence = []
+    # filtered_sentence = []
 
-    for w in word_tokens:
-        if w not in words_to_filter:
-            filtered_sentence.append(w)
+    # for w in word_tokens:
+    #     if w not in words_to_filter:
+    #         filtered_sentence.append(w)
 
-    print("after filtering out words we dont need, you said " + str(filtered_sentence))
+    #print("after filtering out words we dont need, you said " + str(filtered_sentence))
 
     print("these are all the microphone inputs I can find " + str(sr.Microphone.list_microphone_names()))
-
-    print("hello world\n")
---
+++ transcript_custom_ner.py
@@ -0,0 +1,246 @@
+# Load pre-existing spacy model
+import spacy
+nlp=spacy.load('en_core_web_sm')
+# Import requirements
+import random
+from spacy.util import minibatch, compounding
+from pathlib import Path
+import speech_recognition as sr
+import time 
+from spacy import displacy
+
+
+
+
+#from gensim.parsing.preprocessing import remove_stopwords
+#look into finding a way to remove stop words without anaconda, installation issues
+#without using filtering out words library, i still dont catch umms and filler words, so 
+#can just keep it like this?
+
+#using speech_recognition library
+#tutorial: https://realpython.com/python-speech-recognition/
+
+#input as a string
+#microphone low
+def recognize_speech(recognizer, microphone):
+    if not isinstance(recognizer, sr.Recognizer):
+        raise TypeError("`recognizer` must be `Recognizer` instance")
+
+    if not isinstance(microphone, sr.Microphone):
+        raise TypeError("`microphone` must be `Microphone` instance")
+
+    # adjust the recognizer sensitivity to ambient noise and record audio
+    # from the microphone
+    with microphone as source:
+        recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.listen(source)
+
+    # set up the response object
+    response = {
+        "success": True,
+        "error": None,
+        "transcription": None
+    }
+
+    # try recognizing the speech in the recording
+    # if a RequestError or UnknownValueError exception is caught,
+    #     update the response object accordingly
+    try:
+        response["transcription"] = recognizer.recognize_google(audio)
+    except sr.RequestError:
+        # API was unreachable or unresponsive
+        response["success"] = False
+        response["error"] = "API unavailable"
+    except sr.UnknownValueError:
+        # speech was unintelligible
+        response["error"] = "Unable to recognize speech"
+
+    return response
+
+
+if __name__ == "__main__":
+
+   # Getting the pipeline component
+    ner=nlp.get_pipe("ner")
+
+    # training data
+    TRAIN_DATA = [
+                ("Walmart is a leading e-commerce company", {"entities": [(0, 7, "ORG")]}),
+                ("I reached Chennai yesterday.", {"entities": [(19, 28, "GPE")]}),
+                ("I recently ordered a book from Amazon", {"entities": [(24,32, "ORG")]}),
+                ("I was driving a BMW", {"entities": [(16,19, "PRODUCT")]}),
+                ("I ordered this from ShopClues", {"entities": [(20,29, "ORG")]}),
+                ("Fridge can be ordered in Amazon ", {"entities": [(0,6, "PRODUCT")]}),
+                ("I bought a new Washer", {"entities": [(16,22, "PRODUCT")]}),
+                ("I bought a old table", {"entities": [(16,21, "PRODUCT")]}),
+                ("I bought a fancy dress", {"entities": [(18,23, "PRODUCT")]}),
+                ("I rented a camera", {"entities": [(12,18, "PRODUCT")]}),
+                ("I rented a tent for our trip", {"entities": [(12,16, "PRODUCT")]}),
+                ("I rented a screwdriver from our neighbour", {"entities": [(12,22, "PRODUCT")]}),
+                ("I repaired my computer", {"entities": [(15,23, "PRODUCT")]}),
+                ("I got my clock fixed", {"entities": [(16,21, "PRODUCT")]}),
+                ("I got my truck fixed", {"entities": [(16,21, "PRODUCT")]}),
+                ("Flipkart started it's journey from zero", {"entities": [(0,8, "ORG")]}),
+                ("I recently ordered from Max", {"entities": [(24,27, "ORG")]}),
+                ("Flipkart is recognized as leader in market",{"entities": [(0,8, "ORG")]}),
+                ("I recently ordered from Swiggy", {"entities": [(24,29, "ORG")]})
+                ]
+
+    # Adding labels to the `ner`
+
+    for _, annotations in TRAIN_DATA:
+        for ent in annotations.get("entities"):
+            ner.add_label(ent[2])
+
+    # Disable pipeline components you dont need to change
+    pipe_exceptions = ["ner", "trf_wordpiecer", "trf_tok2vec"]
+    unaffected_pipes = [pipe for pipe in nlp.pipe_names if pipe not in pipe_exceptions]
+
+    # training data
+    ## ORG - glasses
+    ## DATE - hair_color
+    ## NORP - hat_color
+    ## GPE = hat
+    ## LAW - bald
+    TRAIN_DATA = [
+                ("Is your person wearing glasses?", {"entities": [(23,30,"ORG")]}),
+                ("Do they have glasses?", {"entities": [(13,20,"ORG")]}),
+                ("Does your person have glasses on?", {"entities": [(22,29,"ORG")]}),
+
+                ("Is your person four-eyed?", {"entities": [(15,24,"ORG")]}),
+                ("Is she four-eyed?", {"entities": [(7,16,"ORG")]}),
+
+                ("Is she blond?", {"entities": [(7,12,"DATE")]}),
+                ("Is your person blond-haired?", {"entities": [(15,20,"DATE")]}),
+                ("Is he golden-haired?", {"entities": [(6,12,"DATE")]}),
+                ("Are they gold-haired?", {"entities": [(9,13,"DATE")]}),
+                ("Does your person have yellow hair?", {"entities": [(22,28,"DATE")]}),
+                ("Are they auburn-haired?", {"entities": [(9,15,"DATE")]}),
+                ("Is your person a ginger?", {"entities": [(17,24,"DATE")]}),              
+                
+                ("Is your person wearing a green hat?", {"entities": [(25,30,"NORP")]}),
+                ("Are they wearing a green hat?", {"entities": [(19,24,"NORP")]}),
+                ("Does your person have a green hat?", {"entities": [(24,29,"NORP")]}), 
+                ("Do they have a green hat?", {"entities": [(15,20,"NORP")]}),
+
+                ("Does your person wear a hat?", {"entities": [(24,27,"GPE")]}), 
+                ("Do they wear a hat?", {"entities": [(15,18,"GPE")]}), 
+                ("Do they have a hat?", {"entities": [(15,18,"GPE")]}), 
+
+                ("Does your person not have head hair?", {"entities": [(17,35,"LAW")]}),
+                ("Is your person bald?", {"entities": [(15,19,"LAW")]}),
+                ("Is she bald?", {"entities": [(7,13,"LAW")]}),
+                ("Is he bald?", {"entities": [(6,10,"LAW")]}),
+                ("Does he not have head hair?", {"entities": [(8,26,"LAW")]}),
+                ("Does he not have hair on his head?", {"entities": [(8,33,"LAW")]})                                          
+                # ("Walmart is a leading e-commerce company", {"entities": [(0, 7, "ORG")]})
+                ]
+
+        # Adding labels to the `ner`
+
+    for _, annotations in TRAIN_DATA:
+        for ent in annotations.get("entities"):
+            ner.add_label(ent[2])
+
+    # Disable pipeline components you dont need to change
+    pipe_exceptions = ["ner", "trf_wordpiecer", "trf_tok2vec"]
+    unaffected_pipes = [pipe for pipe in nlp.pipe_names if pipe not in pipe_exceptions]
+
+
+    # TRAINING THE MODEL
+    with nlp.disable_pipes(*unaffected_pipes):
+
+        # Training for 30 iterations
+        for iteration in range(30):
+
+            # shuufling examples  before every iteration
+            random.shuffle(TRAIN_DATA)
+            losses = {}
+            # batch up the examples using spaCy's minibatch
+            batches = minibatch(TRAIN_DATA, size=compounding(4.0, 32.0, 1.001))
+            for batch in batches:
+                texts, annotations = zip(*batch)
+                nlp.update(
+                            texts,  # batch of texts
+                            annotations,  # batch of annotations
+                            drop=0.5,  # dropout - make it harder to memorise data
+                            losses=losses,
+                        )
+            print("Losses", losses)
+
+
+
+    #auio to text transcription
+
+    recognizer = sr.Recognizer()
+    microphone = sr.Microphone()
+
+    print("these are the stopwords i will use \n")
+    #print(stopwords.words('english'))
+    #words_to_filter = set(stopwords.words('english'))
+    
+    
+
+    instruction = "ask me question based on a specific attribute for my character"
+    print(instruction)
+    time.sleep(1)
+
+    PROMPT_LIMIT = 1 #number of times a user is allowed to speak to microphone
+
+    for i in range(PROMPT_LIMIT):
+        response_from_user = recognize_speech(recognizer, microphone)
+        
+        if not response_from_user["success"]:
+            break
+        print("I didn't catch that. What did you say?\n")
+
+    print("You said: {}".format(response_from_user["transcription"]))
+
+    # word_tokens = word_tokenize(response_from_user["transcription"])
+
+    # filtered_sentence = [w for w in word_tokens if not w.lower() in words_to_filter]
+ 
+    # filtered_sentence = []
+
+    # for w in word_tokens:
+    #     if w not in words_to_filter:
+    #         filtered_sentence.append(w)
+
+    #print("after filtering out words we dont need, you said " + str(filtered_sentence))
+
+    print("these are all the microphone inputs I can find " + str(sr.Microphone.list_microphone_names()))
+
+
+    print("this is actual trascription " + response_from_user["transcription"])
+    transcription = "Do they have blond hair and purple glasses?"
+
+    # Testing the model
+    doc = nlp(transcription)
+    print("Entities", [(ent.text, ent.label_) for ent in doc.ents])
+
+
+    # for ent in doc.ents:
+    # 	print(ent.text, ent.start_char, ent.end_char, ent.label_)
+
+    displacy.render(doc, style='ent',jupyter=True)
+
+    ## ent.text ('blond') and ent.label ('DATE', which will ultimately be 'hair_color') will then be sent to the game backend to check
+    ##
+    ## guess_trait = ent.label
+    ## guess_adj = ent.text
+    ## if guess_trait == guess_adj:
+    ##		return affirmative_response
+
+    # Save the  model to directory
+    output_dir = Path('/content/')
+    nlp.to_disk(output_dir)
+    print("Saved model to", output_dir)
+
+    # Load the saved model and predict
+    print("Loading from", output_dir)
+    nlp_updated = spacy.load(output_dir)
+    doc = nlp_updated("Fridge can be ordered in FlipKart" )
+    print("Entities", [(ent.text, ent.label_) for ent in doc.ents])
+
+
+

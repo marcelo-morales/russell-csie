5c5ee781927a812ac48bccb2961f1d03c4cda019
Rebecca Muratore <rmurato2@jhu.edu>
2022-04-04 15:18:38 -0400

    added russell greeting message to top

--- game_refractor/src/App.js
+++ game_refractor/src/App.js
@@ -7,7 +7,8 @@ function App() {
   return (
     <body>
       <div className="image-container">
-        <p><h1>Try asking questions about:<br></br></h1>
+        <p><h1>Hi, my name is Russell!<br></br></h1>
+        <h1>Try asking questions about:<br></br></h1>
         <h2> Hair color, baldness, hats, glasses</h2></p>
         <img src={dog_avatar} alt="Logo" />
       </div>

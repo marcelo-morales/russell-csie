a8b66bfbab888d5407dee8ba4f55111921900a15
Rebecca Muratore <rmurato2@jhu.edu>
2022-04-06 13:07:59 -0400

    frontend color/positioning adjustments

--- game_refractor/src/App.js
+++ game_refractor/src/App.js
@@ -7,10 +7,10 @@ function App() {
   return (
     <body>
       <div className="image-container">
+        <img src={dog_avatar} alt="Logo" />
         <p><h1>Hi, my name is Russell!<br></br></h1>
         <h1>Try asking questions about:<br></br></h1>
         <h2> Hair color, baldness, hats, glasses</h2></p>
-        <img src={dog_avatar} alt="Logo" />
       </div>
       <Container characters={characters}></Container>
     </body>

2c6aa3018e28e7d45413b567897e33db10692f02
Nicholas Bowen <nbowen3@jhu.edu>
2022-05-03 00:29:19 -0400

    setup text-to-speech to run through flask api instead of http request from frotend

--- api.py
+++ api.py
@@ -1,11 +1,50 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
 from transcript_custom_ner import main
+from google.cloud import texttospeech
+import base64
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/')
 def get_question():
     result = main()
     response = jsonify(result)
-    response.headers.add('Access-Control-Allow-Origin', '*')
+    # response.headers.add('Access-Control-Allow-Origin', '*')
     return response
+
+@app.route('/audio/', methods=['POST'])
+def get_audio():
+    # Instantiates a client
+    client = texttospeech.TextToSpeechClient()
+    text = request.json['text']
+    print(text)
+    # Set the text input to be synthesized
+    synthesis_input = texttospeech.SynthesisInput(text=text)
+
+    # Build the voice request, select the language code ("en-US") and the ssml
+    # voice gender ("neutral")
+    voice = texttospeech.VoiceSelectionParams(
+        language_code="en-US", ssml_gender=texttospeech.SsmlVoiceGender.MALE, name="en-US-Wavenet-A",
+    )
+
+    # Select the type of audio file you want returned
+    audio_config = texttospeech.AudioConfig(
+        audio_encoding=texttospeech.AudioEncoding.MP3
+    )
+
+    # Perform the text-to-speech request on the text input with the selected
+    # voice parameters and audio file type
+    response = client.synthesize_speech(
+        input=synthesis_input, voice=voice, audio_config=audio_config
+    )
+
+    api_response = {
+        "audioContent": str(base64.b64encode(response.audio_content))[2:-1]
+    }
+
+    to_return = jsonify(api_response)
+    # to_return.headers.add('Access-Control-Allow-Origin', '*')
+    return to_return
--- game_refractor/src/services/api.js
+++ game_refractor/src/services/api.js
@@ -2,14 +2,14 @@ import axios from "axios";
 
 const token = "ya29.c.b0AXv0zTODua7uWNhcGQ5WOAaczOUYFvM52icMfqgQpsxIsDfWtkQfeZY_uPvxnUYUMDAbe4kQ5NHXf4o5TOWH3K-FEEIfpAm7Zn-Y3Yjqrt73S0YaUx9nXb49cJQ4rvx2Dsya188hICYSw1_OgPzxRIvNR6H30FOCToSAJrq9QWftrRZpgo68D6TfOX5OVySj1VImowOvylQ5chUode8VeX0SRNhLEXs";
 const axiosInstance = axios.create({
-  baseURL: "https://texttospeech.googleapis.com/v1/text:synthesize",
-  headers: {
-    Authorization: `Bearer ${token}`,
-  },
+  baseURL: "http://127.0.0.1:5000/audio",
+  // headers: {
+  //  Authorization: `Bearer ${token}`,
+  // },
 });
 
 async function getAudio(text) {
-  console.log("this is what i am saying " + text)
+  /*
   const body = {
     input: {
       text: text,
@@ -24,8 +24,12 @@ async function getAudio(text) {
       audioEncoding: "MP3",
     }
   };
+  */
+  const body = {
+    text: text
+  };
   try {
-    const response = await axiosInstance.post('', body);
+    const response = await axios.post('http://127.0.0.1:5000/audio/', body);
     return response.data.audioContent;
   } catch (err) {
     throw err;
